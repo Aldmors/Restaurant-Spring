38aa87ca9dff3fe2f332f6dc1d6ef94ac7b291e9
Aldmor <kubziom.pr@gmail.com>
2025-05-09 20:32:11 +0200

    Add storage and exceptions

---
+++ src/main/java/com/studia/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,14 @@
+package com.studia.restaurant.repositories;
+
+
+import com.studia.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+    // TODO: Custom queries
+
+
+
+}
---
+++ src/main/java/com/studia/restaurant/services/StorageService.java
@@ -0,0 +1,12 @@
+package com.studia.restaurant.services;
+
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface StorageService {
+    String store(MultipartFile file, String folderName);
+    Optional<Resource> loadAsResource(String id);
+}
