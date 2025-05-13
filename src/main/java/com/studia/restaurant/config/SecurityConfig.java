71f93878b066e2958c039a713e07240f81412cbb
Aldmor <kubziom.pr@gmail.com>
2025-05-13 23:23:26 +0200

    Add JavaDocs

--- src/main/java/com/studia/restaurant/config/SecurityConfig.java
+++ src/main/java/com/studia/restaurant/config/SecurityConfig.java
@@ -14,6 +14,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    /**
+     * Configures a {@link SecurityFilterChain} for securing HTTP requests.
+     *
+     * This method defines authorization rules, OAuth2 resource server configuration,
+     * session management policy, and disables CSRF protection.
+     *
+     * @param http the {@link HttpSecurity} object used to configure the HTTP security settings
+     * @return the configured {@link SecurityFilterChain}
+     * @throws Exception if an error occurs while building the security filter chain
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -35,6 +45,13 @@ public class SecurityConfig {
         return http.build();
     }
 
+    /**
+     * Provides a JwtAuthenticationConverter bean for use in JWT-based authentication
+     * within the security configuration. This converter is responsible for converting
+     * a JWT into an Authentication object.
+     *
+     * @return an instance of JwtAuthenticationConverter
+     */
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         return new JwtAuthenticationConverter();
